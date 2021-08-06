26f57c788fefcfaad0e5579507679d4d4f37cf99
KaMeHb-UA <marlock@bassteam.ml>
2021-08-06 21:20:07 +0300

    worker template

--- src/main.go
+++ src/main.go
@@ -4,20 +4,61 @@ import (
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/aws/credentials"
+    "github.com/aws/aws-sdk-go/service/timestreamwrite"
+    "github.com/aws/aws-sdk-go/service/timestreamquery"
+	"net/http"
+	"golang.org/x/net/http2"
+	"time"
+	"net"
+	"os"
+	"strconv"
 )
 
-func createSession() *session.Session {
+func createSession() (*timestreamquery.TimestreamQuery, *timestreamwrite.TimestreamWrite) {
+	tr := &http.Transport{
+		ResponseHeaderTimeout: 20 * time.Second,
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+			Timeout:   30 * time.Second,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+	http2.ConfigureTransport(tr)
+
 	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2"),
-		Credentials: credentials.NewStaticCredentials("AKID", "SECRET_KEY", "TOKEN"),
+		Region: aws.String(os.Getenv("AWS_REGION")),
+		Credentials: credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ""),
+		MaxRetries: aws.Int(10),
+		HTTPClient: &http.Client{ Transport: tr },
 	})
+
 	if err != nil {
 		panic("Cannot create AWS session. Aborting...")
 	}
-	return sess
+
+	return timestreamquery.New(sess), timestreamwrite.New(sess)
+}
+
+func launchListener() {
+	q, w := createSession()
+	//
 }
 
 func main() {
-	sess := createSession()
-	//
+	countStr := os.Getenv("LISTENER_COUNT")
+	count := 10
+	if countStr != "" {
+		converted, err := strconv.Atoi(countStr)
+		if err != nil {
+			count = converted
+		}
+	}
+	for i := 0; i < count; i++ {
+		go launchListener()
+	}
 }
