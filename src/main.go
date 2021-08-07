2f574eb91af1cb2b0238b0347cb4284ce7363af2
KaMeHb-UA <marlock@bassteam.ml>
2021-08-07 15:20:20 +0300

    base query handle

--- src/main.go
+++ src/main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	messagequeue "_/src/mq"
+	timeseries "_/src/ts"
+	"errors"
 	"os"
 	"strconv"
-	timeseries "_/src/ts"
-	messagequeue "_/src/mq"
 )
 
 func launchListener() {
 	ts := timeseries.New()
 	mq := messagequeue.New()
-	mq.Listen(func(data interface {}) interface{} {
-		return data
+	mq.Listen(func(data interface {}) (interface{}, error) {
+		dataTyped, ok := data.(map[string]interface{})
+		if !ok {
+			return nil, errors.New("Cannot convert incoming data to map")
+		}
+		reqType, typeOk := dataTyped["type"].(string)
+		if !typeOk {
+			return nil, errors.New("Cannot get request type")
+		}
+		if reqType == "query" {
+			query, queryOk := dataTyped["data"].(string)
+			if !queryOk {
+				return nil, errors.New("Cannot get request query")
+			}
+			return ts.Query(query)
+		} else if reqType == "write" {
+		} else {
+			return nil, errors.New("Unknown request type " + reqType)
+		}
 	})
 }
 
